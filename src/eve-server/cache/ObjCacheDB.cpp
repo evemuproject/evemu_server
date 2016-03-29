85c724bfd039b81cfc441afdf399117fde60c14a
George <eve-moo-70@gmail.com>
2016-03-29 16:36:32 -0500

    Database update. Changed blkBPTypes to be a table extension for InvBlueprintTypes. reloading the extInvBlueprintTypes.sql file should be the only update needed.

--- src/eve-server/cache/ObjCacheDB.cpp
+++ src/eve-server/cache/ObjCacheDB.cpp
@@ -672,8 +672,9 @@ PyRep *ObjCacheDB::Generate_locationWormholeClasses()
 
 PyRep *ObjCacheDB::Generate_invBlueprintTypes()
 {
+    
     DBQueryResult res;
-    const char *q = "SELECT blueprintTypeID, parentBlueprintTypeID, productTypeID, productionTime, techLevel, researchProductivityTime, researchMaterialTime, researchCopyTime, researchTechTime, productivityModifier, materialModifier, wasteFactor, chanceOfReverseEngineering, maxProductionLimit FROM blkBPTypes";
+    const char *q = "SELECT blueprintTypeID, parentBlueprintTypeID, productTypeID, productionTime, techLevel, researchProductivityTime, researchMaterialTime, researchCopyTime, researchTechTime, productivityModifier, materialModifier, wasteFactor, chanceOfReverseEngineering, maxProductionLimit FROM invBlueprintTypes LEFT JOIN extInvBlueprintTypes Using(blueprintTypeID)";
     if(DBcore::RunQuery(res, q)==false)
     {
         _log(SERVICE__ERROR, "Error in query for cached object 'config.BulkData.bptypes': %s", res.error.c_str());
