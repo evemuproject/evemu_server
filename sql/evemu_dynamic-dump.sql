385319198597cbe6111e53b2825c9e93a6163ae8
George <eve-moo-70@gmail.com>
2016-04-16 02:58:14 -0500

    Fixed incorrectly named sql column.

--- sql/evemu_dynamic-dump.sql
+++ sql/evemu_dynamic-dump.sql
@@ -32,7 +32,7 @@ CREATE TABLE `srvAccount` (
   `banned` tinyint(1) NOT NULL DEFAULT '0',
   `logonCount` int(10) unsigned NOT NULL DEFAULT '0',
   `lastLogin` timestamp NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-  `lastSeconds` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `loginSeconds` bigint(20) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`accountID`),
   UNIQUE KEY `accountName` (`accountName`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
