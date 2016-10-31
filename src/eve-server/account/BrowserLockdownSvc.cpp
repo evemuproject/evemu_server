8c560a7f846f22b415c3cb61b0518489468955be
George <eve-moo-70@gmail.com>
2016-10-31 08:16:14 -0500

    Send correct flagged sites hash.

--- src/eve-server/account/BrowserLockdownSvc.cpp
+++ src/eve-server/account/BrowserLockdownSvc.cpp
@@ -68,70 +68,22 @@ PyString:"util.CachedObject"
     return new PyObject( "util.CachedObject" , arg_tuple );
 }
 
-/**
- * CacheOK python class. This class simulates the python class CacheOK which is derived from the StandardError class.
- */
-class CacheOK : public PyObjectEx_Type1
-{
-public:
-    CacheOK() : PyObjectEx_Type1( new PyToken("objectCaching.CacheOK"), new_tuple("CacheOK") ) {}
-};
-
-
-
 PyResult BrowserLockdownService::Handle_GetFlaggedSitesHash(PyCallArgs &call)
 {
-    /* if cache hash is correct send CacheOK */
-
-    /**
-      PyTuple:4
-        itr[0]:PyInt(0x1)
-        itr[1]:"GetFlaggedSitesHash"
-        itr[2]:PyTuple:0
-        itr[3]:PyDict:1
-          dict["machoVersion"]=PyList:2
-            itr:PyLong(0x1CC26986D4B75A0) // time
-            itr:PyInt(0xC017) // hash?
-     */
-    return new CacheOK();
+    // Future updates should be the md5 sum of the cache/browser/flaggedsites.dat file
+    // from user/appdata/Local/CCP/EVE
+    // Send md5 sum for an empty set.
+    return new PyString("d751713988987e9331980363e24189ce");
 }
 
-PyResult BrowserLockdownService::Handle_GetFlaggedSitesList(PyCallArgs &call) {
-
+PyResult BrowserLockdownService::Handle_GetFlaggedSitesList(PyCallArgs &call)
+{
     //PyDict* args = new PyDict;
-
     //return new PyObject( "objectCaching.CachedMethodCallResult", args );
-//}
-
-        /*PyClass
-          PyString:"objectCaching.CachedMethodCallResult"
-          PyTuple:3
-            itr[0]:PyDict:1
-              dict["versionCheck"]=PyTuple:3
-                itr[0]:"run"
-                itr[1]:"run"
-                itr[2]:"run"
-            itr[1]:PyClass
-              PyString:"util.CachedObject"
-              PyTuple:3
-                itr[0]:PyTuple:3
-                  itr[0]:"Method Call"
-                  itr[1]:"server"
-                  itr[2]:PyTuple:2
-                    itr[0]:"browserLockdownSvc"
-                    itr[1]:"GetFlaggedSitesHash"
-                itr[1]:0xACECD
-                itr[2]:PyTuple:2
-                  itr[0]:0x1CC26986D4B75A0
-                  itr[1]:0xC017
-            itr[2]:None
-            */
-
-
     PyTuple* arg_tuple = new PyTuple(3);
 
-        PyDict* itr_1 = new PyDict();
-        itr_1->SetItem("versionCheck", new_tuple("run", "run", "run"));
+    PyDict* itr_1 = new PyDict();
+    itr_1->SetItem("versionCheck", new_tuple("run", "run", "run"));
 
     arg_tuple->SetItem(0, itr_1);
     arg_tuple->SetItem(1, GenerateLockdownCachedObject());
