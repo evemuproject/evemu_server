500be37e8ca18b28dfb0b83848a67aac8ae56133
James Hirst <jdhirst@hirstgroup.net>
2021-03-26 17:01:07 +0100

    Updated database and sql installation scripts
    This commit removes old sql installation scripts and organizes everything into a logical structure.
    
    Additionally, for all tables to be added at install time, we are now using GZip for storage in the repository to save space and not require the use of LFS.
    
    The database sql files have also been updated so that they now match the latest eve-server core updates in 0.8.0.
    Update SQL table data
    
    This commit updates SQL table data with new schema changes made in 0.8.0

--- sql/install.sh
+++ sql/install.sh
@@ -5,14 +5,12 @@ port="3306"     #Database Port
 user="evemu"     #Database Username
 pass="evemu"     #Database Password
 
-###CHANGE ME BACK TO evemu###
 database="evemu"   #Database name
 
 tables=(`find tables/. -name "*.sql.gz"`)
 for i in ${!tables[*]}
 do
     echo " >> Installing.. ($(($i+1))/${#tables[@]})  ${tables[$i]}"
-    #mysql -h ${host} --user=${user} --port=${port} --password=${pass} ${database} < ${tables[$i]}
     zcat ${tables[$i]} | mysql -h ${host} --user=${user} --port=${port} --password=${pass} ${database}
 
 done
